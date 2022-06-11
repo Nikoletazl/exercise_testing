2ea8cf90eba20f9a9a82a881493639aeb41d4232
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-06-11 23:12:42 +0300

    first commit

---
+++ tests/charLookUp.js
@@ -0,0 +1,35 @@
+let expect = require('chai').expect
+function lookupChar(string, index) {
+
+    if (typeof(string) !== 'string' || !Number.isInteger(index)) {
+    
+    return undefined;
+    
+    }
+    
+    if (string.length <= index || index < 0) { return "Incorrect index"; } return string.charAt(index); }
+
+
+describe('test', () => {
+    it('check wheter first param type is valid', () => {
+        expect(lookupChar(0, 0)).to.equal(undefined);
+    });
+
+    it('check wheter second param type is valid', () => {
+        expect(lookupChar('hello', 'test')).to.equal(undefined);
+    });
+
+    it('check whether second param is valid integer', () => {
+        expect(lookupChar('hello', 0.1)).to.equal(undefined);
+    });
+    it('check value with index bigger than string length', () => {
+        expect(lookupChar('hi', 10)).to.equal('Incorrect index');
+    });
+    it('check value with negative index', () => {
+        expect(lookupChar('hi', -10)).to.equal('Incorrect index');
+    });
+
+    it('check value with valid params', () => {
+        expect(lookupChar('hello', 1)).to.equal('e');
+    });
+})
